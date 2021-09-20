b91fa0ccec7a0dda130e778da31b52fdc2e0149b
Ryosuke Okuda <ryosuke.okuda@clould.com>
2021-09-20 11:58:24 +0900

    Initial Commit

---
+++ ARSample/ARSample-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ARSample-Bridging-Header.h
+//  ARSample
+//
+//  Created by 奥田亮輔 on 2021/09/20.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
